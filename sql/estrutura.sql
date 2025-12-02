51e2b7352d2028417c4481d0bca382f3a5095479
Gelcineypc <gelcineycorrea@unipam.edu.br>
2025-12-02 01:19:40 +0000

    Criada a tabela funcionario

--- sql/estrutura.sql
+++ sql/estrutura.sql
@@ -6,3 +6,9 @@ CREATE TABLE combustivel (
   estoque_l NUMERIC(12,3) NOT NULL DEFAULT 0,
   CONSTRAINT combustivel_nome_uniq UNIQUE (nome)
 );
+
+CREATE TABLE funcionario (
+  funcionario_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  nome VARCHAR(150) NOT NULL,
+  ativo BOOLEAN NOT NULL DEFAULT true
+);
