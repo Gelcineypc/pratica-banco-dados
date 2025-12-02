1e385e1eba1159fa695b65bd7e2a44de036b57f2
Gelcineypc <gelcineycorrea@unipam.edu.br>
2025-12-02 01:16:09 +0000

    Criada tabela combustivel

--- sql/estrutura.sql
+++ sql/estrutura.sql
@@ -1,1 +1,8 @@
 CREATE DATABASE posto_combustivel;
+
+CREATE TABLE combustivel (
+  combustivel_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  estoque_l NUMERIC(12,3) NOT NULL DEFAULT 0,
+  CONSTRAINT combustivel_nome_uniq UNIQUE (nome)
+)
